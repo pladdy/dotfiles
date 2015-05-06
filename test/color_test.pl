0381cdded63bf684d7d53833a22fe05517b49338
Matt Pladna <mpladna@lgscout.com>
2015-05-06 17:38:57 -0400

    added/moved test files

---
+++ test/color_test.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+##
+ # this is just a test, only a test, of vim colors.  yay colors!!!
+ ##
+
+my $integer = 1;
+my $string = "test-niner";
+
+my @array = [1, 2, "test"];
+
+sub test_sub {
+  return 1;
+}
