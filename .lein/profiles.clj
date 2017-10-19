fad0eea80f713c2532f851d77cfe31b22d954e92
Matt Pladna <pladdypants@gmail.com>
2017-10-18 20:41:13 -0400

    add new files to install

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,3 +1,3 @@
 {:user {:plugins [[lein-exec "0.3.5"]
-                  [cider/cider-nrepl "0.10.2"]]
+                  [cider/cider-nrepl "0.14.0"]]
         :dependencies [[org.clojure/core.async "0.1.346.0-17112a-alpha"]]}}
